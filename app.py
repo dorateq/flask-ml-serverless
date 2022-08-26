787c2678552ea45e0098be07bbc602685267a968
Jonathan Nmaju <kudomago@gmail.com>
2022-08-26 19:34:09 +0000

    Edit app.py, change API message

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home ci cd pipelines</h3>"
+    html = "<h3>Sklearn Prediction Home ci cd pipelines with Flask</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value!
